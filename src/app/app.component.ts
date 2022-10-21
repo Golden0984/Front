8c090fbce281e1c52804a9112b585fbcc1704897
Golden0984 <manishthakur098@hotmail.com>
2022-10-21 15:28:48 +0200

    primer

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+//IMPORTO EL ARCHIVO JSON
+import info from 'src/assets/json/info.json';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Version-0';
+  Informacion: any = info;
+  
+}
