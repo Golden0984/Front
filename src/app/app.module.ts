a17b9cab2c434b0bf9d169e4fa27765f25f6359f
strom99 <alissoncc17@gmail.com>
2022-10-30 20:48:56 +0100

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MembersComponent } from './components/members/members.component';
+import { InfoContactComponent } from './components/info-contact/info-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MembersComponent,
+    InfoContactComponent
   ],
   imports: [
     BrowserModule,
